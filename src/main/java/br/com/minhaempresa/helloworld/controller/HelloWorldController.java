998f8d9165fd85fd0856b00844c17cb69f7a4b0b
Wanderson Xesquevixos de Siqueira <wanderson.siqueira@mandae.com.br>
2022-08-25 20:11:04 -0300

    Aula POST

--- src/main/java/br/com/minhaempresa/helloworld/controller/HelloWorldController.java
+++ src/main/java/br/com/minhaempresa/helloworld/controller/HelloWorldController.java
@@ -1,9 +1,10 @@
 package br.com.minhaempresa.helloworld.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.minhaempresa.helloworld.model.Pessoa;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloWorldController {
@@ -18,6 +19,19 @@ public class HelloWorldController {
     public String helloWorld2(@RequestParam(value = "nome", required = false ) String nome){
         System.out.println("PASSEI AQUI 2");
         return "Hello World! Tudo bem " + nome + " ?";
+    }
+
+    @RequestMapping(value = "pessoas/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Pessoa> getPessoaPorId(@PathVariable(value = "id") Long id){
+        return ResponseEntity.ok(new Pessoa(id, "Wanderson Server" , 35));
+    }
+
+    @RequestMapping(value = "pessoas", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Pessoa> criarPessoa(@RequestBody Pessoa pessoa){
+        pessoa.setId(10000L);
+        pessoa.setNome(pessoa.getNome() + " SERVER ");
+        pessoa.setIdade(pessoa.getIdade() + 50);
+        return new ResponseEntity(pessoa, HttpStatus.CREATED);
     }
 
 }
---
+++ src/main/java/br/com/minhaempresa/helloworld/model/Pessoa.java
@@ -0,0 +1,43 @@
+package br.com.minhaempresa.helloworld.model;
+
+public class Pessoa {
+
+    private Long id;
+
+    private String nome;
+
+    private Integer idade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Pessoa() {
+    }
+
+    public Pessoa(Long id, String nome, Integer idade) {
+        this.id = id;
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+}
